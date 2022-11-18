af86767071d19d41c658351ea67c4ee1e5382859
Anubhav\-Patel <anubhavpatel099@gmail.com>
2022-11-18 23:36:49 +0530

    Added Loading Screen

--- core/src/com/mygdx/game/MainMenu.java
+++ core/src/com/mygdx/game/MainMenu.java
@@ -2,90 +2,67 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-import java.io.FileReader;
-
-public class MainMenu implements Screen {
-
-    final TankStars game;
-    private Texture backgroundImage;
-    private TextureRegion backgroundTexture;
+public class MainMenu implements Screen{
+    final TankStars tankStars;
     OrthographicCamera camera;
+    private TextureRegion backgroundTexture;
+    private Texture backgroundImage;
 
-    public MainMenu(final TankStars game) {
-        this.game = game;
-        backgroundImage = new Texture(Gdx.files.internal("armored_by_wojtekfus_dbl2y6q-pre.jpg"));
-        backgroundTexture = new TextureRegion(backgroundImage, 0, 0, 1236, 600);
+    public MainMenu(final TankStars tankStars) {
+        this.tankStars = tankStars;
+
+        backgroundImage = new Texture(Gdx.files.internal("4k_default_blue_windows_8_solid_wallpaper_by_duning_d7202y2.png"));
+        backgroundTexture = new TextureRegion(backgroundImage, 0, 0, 1192, 670);
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
     }
 
     @Override
-    public void show() {
-
-    }
-
-    @Override
     public void render(float delta) {
-//        FileHandle in = new FileHandle("font.otf");
         ScreenUtils.clear(0, 0, 0, 0);
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("SeventiesGroovy-owZ7q.ttf"));
-        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        FreeTypeFontGenerator.FreeTypeFontParameter parameter2 = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 40;
-        parameter2.size = 20;
-        BitmapFont font1 = generator.generateFont(parameter);
-        BitmapFont font2 = generator.generateFont(parameter2);
 
+        // tell the camera to update its matrices.
         camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
-        game.batch.begin();
-        game.font.setColor(1, 0, 0, 1);
-        game.batch.draw(backgroundTexture, 0,0, 800, 480);
-        font1.draw(game.batch, "Welcome to Tank Stars!", 200, 340);
-        font2.draw(game.batch, "Click anywhere to begin!", 300, 240);
-        generator.dispose();
-        game.batch.end();
 
+        // tell the SpriteBatch to render in the
+        // coordinate system specified by the camera.
+        tankStars.batch.setProjectionMatrix(camera.combined);
 
-        if (Gdx.input.isTouched()) {
-            game.setScreen(new Game(game));
-            dispose();
-        }
+        // begin a new batch and draw the bucket and
+        // all drops
+        tankStars.batch.begin();
+        tankStars.batch.draw(backgroundTexture, 0,0, 800, 480);
+        tankStars.batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-
     }
 
     @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
+    public void show() {
+        // start the playback of the background music
+        // when the screen is shown
     }
 
     @Override
     public void hide() {
-
     }
 
     @Override
-    public void dispose() {
-
+    public void pause() {
     }
 
-}
+    @Override
+    public void resume() {
+    }
 
+    @Override
+    public void dispose() {}
+}
--- core/src/com/mygdx/game/TankStars.java
+++ core/src/com/mygdx/game/TankStars.java
@@ -1,12 +1,9 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class TankStars extends Game {
 	SpriteBatch batch;
@@ -17,8 +14,7 @@ public class TankStars extends Game {
 	public void create () {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
-		img = new Texture("background3.jpg");
-		this.setScreen(new MainMenu(this));
+		this.setScreen(new LoadingScreen(this));
 	}
 
 	@Override
@@ -29,6 +25,5 @@ public class TankStars extends Game {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
