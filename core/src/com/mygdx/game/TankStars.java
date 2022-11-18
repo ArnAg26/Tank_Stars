6db17d527bf91956b163304b9a973d759a93e912
Anubhav\-Patel <anubhavpatel099@gmail.com>
2022-11-18 15:49:02 +0530

    Project

---
+++ core/src/com/mygdx/game/TankStars.java
@@ -0,0 +1,34 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class TankStars extends Game {
+	SpriteBatch batch;
+	BitmapFont font;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		img = new Texture("background3.jpg");
+		this.setScreen(new MainMenu(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
